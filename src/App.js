d9216c4f964ea98ca419c0454fd1430dea7e43eb
piaspro <golamrahman09@gmail.com>
2020-08-24 10:51:57 +0600

    complited

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState([]);
+  useEffect(()=>{
+    fetch('https://randomuser.me/api/?results=5000')
+    .then(response => response.json())
+    .then(data => setUser((data.results).slice(0, 20)))
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {
+          user.map(person => <User gender = {person.gender}
+            title = {person.name.title}
+            firstName = {person.name.first}
+            lastName = {person.name.last}
+            email = {person.email}
+            img = {person.picture.large}
+            phone = {person.phone}
+          ></User>)
+        }
     </div>
   );
 }
+function User(props) {
+  return(
+    <div className="user">
+          <img src={props.img} alt=""/>
+          <h2>Name: {props.title}  {props.firstName}  {props.lastName}</h2>
+          <h3>Email: {props.email}</h3>
+          <h4>Gender: {props.gender}</h4>
+          <p>Phone: <strong>{props.phone}</strong></p>
+    </div>
+  )
+}
+
+
 
 export default App;
